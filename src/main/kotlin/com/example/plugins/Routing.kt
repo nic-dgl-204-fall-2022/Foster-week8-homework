36929de4faaa3262484792e4957393eb6b384f4b
YoukaiDragon <tyson.foster@hotmail.com>
2022-11-17 10:34:38 -0800

    Show a list of articles

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,9 +1,11 @@
 package com.example.plugins
 
+import com.example.models.articles
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 
@@ -18,7 +20,7 @@ fun Application.configureRouting() {
         }
         route("articles") {
             get {
-                // Show a list of articles
+                call.respond(FreeMarkerContent("index.ftl", mapOf("articles" to articles)))
             }
             get("new") {
                 // Show a page with fields for creating a new article
