81395262564ae9da22ee669d162e854433ac58e5
YoukaiDragon <tyson.foster@hotmail.com>
2022-11-17 10:16:47 -0800

    Adjust FreeMarker Configuration

--- src/main/kotlin/com/example/plugins/Templating.kt
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import freemarker.cache.*
+import freemarker.core.HTMLOutputFormat
 import io.ktor.server.freemarker.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
@@ -10,6 +11,7 @@ import io.ktor.server.request.*
 fun Application.configureTemplating() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
     }
     routing {
     }
